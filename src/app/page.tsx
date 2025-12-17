7a8ab2242802ba08ae511a4f392a9e34ff357928
AugustineOjeh <ao.ojeh@gmail.com>
2025-12-17 09:12:10 +0100

    Global styles defined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,21 +13,21 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-gilroy font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-s text-h1 font-gilroy text-foreground">
             To get started, edit the page.tsx file.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 font-aeonik dark:text-zinc-400">
+          <p className="max-w-md text-xlarge leading-8 text-foreground font-aeonik">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium font-aeonik text-zinc-950 dark:text-zinc-50"
+              className="font-bold font-aeonik text-link"
             >
               Templates
             </a>{" "}
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium font-aeonik text-zinc-950 dark:text-zinc-50"
+              className="font-bold font-aeonik text-link"
             >
               Learning
             </a>{" "}
@@ -36,13 +36,13 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background font-gilroy font-semibold transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-primary px-5 text-foreground font-gilroy font-semibold transition-colors hover:bg-primary-hover md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
+              // className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
